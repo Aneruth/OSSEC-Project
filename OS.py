7e561860cf0e5aea461c8870df654e9de1f034c4
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-04-05 12:26:19 +0200

    rendered the code

--- OS.py
+++ OS.py
@@ -1,4 +1,4 @@
-def bankersAlgorithm(allocation,maximum_allowance): 
+def needMatrix(allocation,maximum_allowance): 
     need = []
     for i in range(len(allocation)):
         for j in range(len(allocation[0])):
@@ -7,62 +7,54 @@ def bankersAlgorithm(allocation,maximum_allowance):
     for i in result:
         need.append(i)
     
+    return need
+    
+def isSafe(P,R):
     # Considering all process are incomplete.
     # The allocation provides us the number of process present. This can be done by considering the list of our allocation matrix.
-    done = [0] * len(allocation) 
-    
-    # To print safe sequence for our problem
-    # This safety sequence provides us which all process are safe
-    # Initially we are assigning our safe sequence as 0 with the length of our allocation. 
-    safe_sequence = [0] * len(allocation)
-
-    pointer = 0
-
-    while (pointer < len(allocation)):
-        find = False
-
-        for i in range(len(allocation)):
-
-            for j in range(len(work)):
-                # Check if need >= work
-                # If its' greater than work then return the pointer variable as 1 and break the loop
-                if need[i][j] > work[j]:
-                    pointer = 1
-                    break
-            
-                else:
-                # Check if need <= work
-                # update available += allocation
-                    for k in range(len(work)):
-                        work[k] += allocation[i][k]
-                
-                safe_sequence[i] = i
-                pointer += 1
-                done[i] = 1
-                find = True
-
-        if find == False:
-            return f"Is system in safe state: {False}"
-    
-    print("Eureka!!!! We found the safe sequence for out given process and their sequece is: \n", end = " ")
-    print(*safe_sequence) 
+    done = [0] * P
+    Sequence = [0] * P
+    count = 0
+    while( count < P ):
+        temp=0
+        for i in range( P ):
+            if( needMatrix(allocation,maximum_allowance)[i] == 0 ):
+                if(needMatrix(allocation,maximum_allowance)(i)):
+                    Sequence[count]=i;
+                    count+=1
+                    done[i]=1
+                    temp=1
+                    for j in range(R):
+                        work[j] += allocation[i][j] 
+        if(temp == 0):
+            break
+    if(count < P):
+        print('The system is Unsafe')
+    else:
+        print("Eureka!!!! We found the safe sequence for out given process and their sequece is: \n", end = " ")
+        print("Safe Sequence: ",*Sequence)
+        print("Available Resource:",work)
     return True
 
 if __name__ == "__main__":
-    allocation = [[0,1,0],[2,0,0],[3,0,2],[2,1,1],[0,0,2]] # consist of 2d list 
+    allocation = [[4,0,0,1],[1,1,0,0],[1,2,5,4],[0,6,3,3],[0,2,1,2]] # consist of 2d list 
     # Each inner list consist of a process 
 
-    maximum_allowance = [[7,5,3],[3,2,2],[9,0,2],[2,2,2],[4,3,3]] # a 2d matrix 
+    maximum_allowance = [[6,0,1,2],[1,7,5,0],[2,3,5,6],[1,6,5,3],[1,6,5,6]] # a 2d matrix 
     # Each inner list consist of a process 
 
-    result = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    result = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     
     # Define a function to calcualte the value from allcoation matrix
     # Assuming values of each process and subtracting it with our available.
     # return available as as list
     rowSum_allocation = [sum(i) for i in zip(*allocation)]  
-    total = [10,5,7]
+    total = [10,5,7,3]
     work = [i-j for i,j in zip(total,rowSum_allocation)]
-    # print(result)
+    P = len(allocation) # Defines number of process
+    R = len(allocation[0]) # Defones number if resources
+
     # Calling our methods
-    print(bankersAlgorithm(allocation,maximum_allowance))
+    print('The need matrix is: ')
+    print(needMatrix(allocation,maximum_allowance))
+    print(isSafe(P,R))
